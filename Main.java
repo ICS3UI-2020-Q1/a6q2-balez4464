5e3990b21ac2a97855a54944a0b831f790b191df
balez4464 <balez4464@wrdsb.ca>
2020-10-08 13:54:28 +0000

    Created a program that seachers 10 numbers for the number of the users choosing

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program allows the user to enter numbers and finds the numbers in the array
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +10,39 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user to put in numbers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //creates a constant for the array
+    final int NUM_AMOUNT = 10;
+
+    //creates array for the nummebrs
+    int numbers[] = new int[NUM_AMOUNT];
+
+    //for loop so user can write down their numbers
+    for (int i = 0; i < numbers.length; i++){
+      //allows them to print in thier numbers
+      numbers[i] = input.nextInt();
+    }
+
+    //asks to find number
+    System.out.println("Please enter a number to find");
+
+    //allows user to pick a number
+    int searchNumber = input.nextInt();
+
+    //for loop to go thorugh and check
+    for (int i = 0; i < numbers.length; i++){
+      //prints if the index is equal to search number
+      if(numbers[i] == searchNumber){
+        //prints what index its found at
+        System.out.println(searchNumber + " is located at index " + i);
+      }
+
+    }
+
   }
 }
